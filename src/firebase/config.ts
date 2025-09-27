a14c8740202f66573b042204ba862c41ad0d261e
Philip Onyango <onyangophilip244@gmail.com>
2025-09-27 11:53:56 +0000

    Try fixing this error: `Console Error: Error: Failed to execute 'open' o

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,6 @@
 // src/firebase/config.ts
+import 'dotenv/config';
+
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
