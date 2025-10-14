6cb3f88ebf7bc54f015da16c28a764c8d507e942
Philip Onyango <onyangophilip244@gmail.com>
2025-10-14 19:37:49 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyA5P7X7o_..._7o4Y1eT8",
+  apiKey: "AIzaSyBwmM9O7IHzymVw6uc7j_8Ft7uY3klTp48",
   authDomain: "chattyclone-5lnof.firebaseapp.com",
   projectId: "chattyclone-5lnof",
   storageBucket: "chattyclone-5lnof.appspot.com",
   messagingSenderId: "469848693222",
-  appId: "1:469848693222:web:a9e334a75c734316d27439",
-  measurementId: "G-8B4K2D5B5B"
+  appId: "1:469848693222:web:9f10eab8ea7a2c99a11e27"
 };
