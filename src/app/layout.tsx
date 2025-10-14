a74232794c32f1002851dce2da4bee48ea3a63df
Philip Onyango <onyangophilip244@gmail.com>
2025-10-14 19:14:40 +0000

    yes the firebase is currently unnavailable so we are just testing it unt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
-import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
   title: 'Philip Virtual Assistant',
@@ -23,9 +22,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#000000" />
       </head>
       <body className="font-body antialiased">
-        <FirebaseClientProvider>
-          {children}
-        </FirebaseClientProvider>
+        {children}
         <Toaster />
       </body>
     </html>
